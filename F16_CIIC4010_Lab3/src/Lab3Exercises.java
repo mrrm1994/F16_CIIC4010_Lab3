a758ad7aa7a28397bd6b6b84dbdc30b82a59c66c
4015d15 <4015d15@guineo.ece.uprm.edu>
2016-08-31 17:55:55 -0400

    First 4 questions of the third lab ICOM 4015

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -1,11 +1,17 @@
 import javax.swing.JFrame;
 
-public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+public class Lab3Exercises
+
+{
+	public static void main(String[] args) 
+	
+	{
+		JFrame myFrame = new JFrame(" Michael ");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(900, 400);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		
 	}
+	
 }
