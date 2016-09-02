10b7557e8618fdc1a8f94fcb7e64d587aaf4b9d7
4015d15 <4015d15@guineo.ece.uprm.edu>
2016-09-01 21:31:17 -0400

    All 9 questions of the third lab ICOM 4015 (Finished)

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -9,7 +9,7 @@ public class Lab3Exercises
 		JFrame myFrame = new JFrame(" Michael ");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 400);
-		myFrame.setSize(200, 200);
+		myFrame.setSize(300, 152);
 		
 		MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -28,47 +28,89 @@ public class MyPanelClass extends JPanel
 		int height = y2 - y1;
 
 		
-		//Paint the background
-		g.setColor(Color.MAGENTA);
-		g.fillRect(x1, y1, width + 1, height + 1);
-//		 //Draw a border
-//        g.setColor(Color.YELLOW);
-//        g.drawRect(x1, y1, width, height);
-//        // Inner border
-//        g.setColor(Color.GREEN);
-//        g.drawRect(x1 +5, y1 + 5, width -10, height -10);
-//        g.setColor(Color.WHITE);
-//        g.drawLine(x1, y1, x2, y2);
-//        g.setColor(Color.BLACK);
-//        g.drawLine(x1, y2,x2,y1);
+			g.setColor(Color.RED);
+	        g.fillRect(x1, y1, width+1, height+1);
+	             
+	         
+	        g.drawRect(0, y1+30, width+1,y1+ 30);
+	        g.setColor(Color.WHITE);
+	        g.fillRect(0, y1+30, width+1, y1+30);
+	        
+	        g.drawRect(0, y1+90, width+1, y1+30);
+	        g.setColor(Color.WHITE);
+	        g.fillRect(0, y1+90, width+1, y1+30);
+	            
+	        
+	        Polygon p1 = new Polygon();
+	        p1.addPoint(x1 , y1 );
+	        p1.addPoint(x1 + 115, y1 + 73);
+	        p1.addPoint(x1 -10, y1 + 158);
 
-//		   g.setColor(Color.LIGHT_GRAY);
-//         g.fillOval((x2-55)/2, (y2-55)/2, 55, 55);
-		
-//		Polygon p = new Polygon();
-//        p.addPoint(x1 + 5, y1 + 25);
-//        p.addPoint(x1 + 20, y1 + 10);
-//        p.addPoint(x1 + 35, y1 + 25);
-//        p.addPoint(x1 + 25, y1 + 25);
-//        p.addPoint(x1 + 25, y1 + 45);
-//        p.addPoint(x1 + 15, y1 + 45);
-//        p.addPoint(x1 + 15, y1 + 25);
-//        g.setColor(Color.YELLOW);
-//        g.fillPolygon(p);
-		
-		 Polygon p2 = new Polygon();
-         p2.addPoint(x1 + 25, y1 + 73);
-         p2.addPoint(x1 + 41, y1 + 73);
-         p2.addPoint(x1 + 47, y1 + 58);
-         p2.addPoint(x1 + 53, y1 + 73);
-         p2.addPoint(x1 + 69, y1 + 73);
-         p2.addPoint(x1 + 56, y1 + 83);
-         p2.addPoint(x1 + 61, y1 + 98);
-         p2.addPoint(x1 + 47, y1 + 88);
-         p2.addPoint(x1 + 34, y1 + 98);
-         p2.addPoint(x1 + 38, y1 + 83);
-         g.setColor(Color.WHITE);
-         g.drawPolygon(p2);
+	        g.setColor(Color.BLUE);
+	        g.fillPolygon(p1);
+
+
+	        Polygon p2 = new Polygon();
+	        p2.addPoint(x1 + 25, y1 + 73);
+	        p2.addPoint(x1 + 41, y1 + 73);
+	        p2.addPoint(x1 + 47, y1 + 58);
+	        p2.addPoint(x1 + 53, y1 + 73);
+	        p2.addPoint(x1 + 69, y1 + 73);
+	        p2.addPoint(x1 + 56, y1 + 83);
+	        p2.addPoint(x1 + 61, y1 + 98);
+	        p2.addPoint(x1 + 47, y1 + 88);
+	        p2.addPoint(x1 + 34, y1 + 98);
+	        p2.addPoint(x1 + 38, y1 + 83);
+	        g.setColor(Color.WHITE);
+
+	        Polygon p3 = p2;
+	        p3.translate(-5, -10);
+	        g.fillPolygon(p3);
+	        
+	        //The commented lines below are from the previous questions of the third lab
+	    
+	    	//Paint the background
+//			g.setColor(Color.MAGENTA);
+//			g.fillRect(x1, y1, width + 1, height + 1);
+//			 //Draw a border
+//	        g.setColor(Color.YELLOW);
+//	        g.drawRect(x1, y1, width, height);
+//	        // Inner border
+//	        g.setColor(Color.GREEN);
+//	        g.drawRect(x1 +5, y1 + 5, width -10, height -10);
+//	        g.setColor(Color.WHITE);
+//	        g.drawLine(x1, y1, x2, y2);
+//	        g.setColor(Color.BLACK);
+//	        g.drawLine(x1, y2,x2,y1);
+
+//			   g.setColor(Color.LIGHT_GRAY);
+//	         g.fillOval((x2-55)/2, (y2-55)/2, 55, 55);
+			
+//			Polygon p = new Polygon();
+//	        p.addPoint(x1 + 5, y1 + 25);
+//	        p.addPoint(x1 + 20, y1 + 10);
+//	        p.addPoint(x1 + 35, y1 + 25);
+//	        p.addPoint(x1 + 25, y1 + 25);
+//	        p.addPoint(x1 + 25, y1 + 45);
+//	        p.addPoint(x1 + 15, y1 + 45);
+//	        p.addPoint(x1 + 15, y1 + 25);
+//	        g.setColor(Color.YELLOW);
+//	        g.fillPolygon(p);
+			
+//			 Polygon p2 = new Polygon();
+//	         p2.addPoint(x1 + 25, y1 + 73);
+//	         p2.addPoint(x1 + 41, y1 + 73);
+//	         p2.addPoint(x1 + 47, y1 + 58);
+//	         p2.addPoint(x1 + 53, y1 + 73);
+//	         p2.addPoint(x1 + 69, y1 + 73);
+//	         p2.addPoint(x1 + 56, y1 + 83);
+//	         p2.addPoint(x1 + 61, y1 + 98);
+//	         p2.addPoint(x1 + 47, y1 + 88);
+//	         p2.addPoint(x1 + 34, y1 + 98);
+//	         p2.addPoint(x1 + 38, y1 + 83);
+//	         g.setColor(Color.WHITE);
+//	         g.drawPolygon(p2);
+	        
 	}
 	
 }
